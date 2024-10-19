9618fc312b475ea51581881dfac75e619bf83956
Andres Granda <andres.granda@tafitech.io>
2024-10-19 11:25:55 -0500

    Mostrando detalle en carro de compra

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,21 @@
 import Link from "next/link";
 import { ShoppingCartIcon } from "@heroicons/react/20/solid";
 import { useCartContext } from "../CartContext/CartContext";
+import { useEffect, useState } from "react";
 
 export function Navbar() {
   const { productos } = useCartContext();
-
+  const [distinctProducts, setDistinctProducts] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const toggleCart = () => setIsCartOpen(!isCartOpen);
+  useEffect(() => {
+    setDistinctProducts(
+      productos.filter(
+        (producto, index, self) =>
+          index === self.findIndex((p) => p.name === producto.name)
+      )
+    );
+  }, [productos]);
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto">
@@ -17,7 +28,10 @@ export function Navbar() {
               <Link href="/nosotros">Nosotros</Link>
               <Link href="/detalleCompra">Detalle compra</Link>
               <div className="relative">
-                <button className="text-white hover:text-gray-400">
+                <button
+                  onClick={toggleCart}
+                  className="text-white hover:text-gray-400"
+                >
                   <ShoppingCartIcon className="w-6 h-6" />
                   {productos.length > 0 && (
                     <span className="absolute top-2 right-2 bg-red-600 text-white rounded-full w-5 h-5">
@@ -25,6 +39,45 @@ export function Navbar() {
                     </span>
                   )}
                 </button>
+                {isCartOpen && (
+                  <div className="absolute right-0 mt-2 w-64 bg-white shadow-lg rounded-lg p-4">
+                    {distinctProducts.length > 0 ? (
+                      <ul>
+                        {distinctProducts.map((product, index) => (
+                          <li
+                            key={index}
+                            className="border-b py-2 text-gray-800 flex justify-between"
+                          >
+                            <span>
+                              {product.name}
+                              {productos.filter(
+                                (prod) => prod.name === product.name
+                              ).length !== 0
+                                ? ` x ${
+                                    productos.filter(
+                                      (prod) => prod.name === product.name
+                                    ).length
+                                  }`
+                                : ""}
+                            </span>
+                            <span>
+                              {productos
+                                .filter((prod) => prod.name === product.name)
+                                .map((prod) => prod.price)
+                                .reduce(
+                                  (acumulador, valorActual) =>
+                                    acumulador + valorActual,
+                                  0
+                                )}
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <p className="text-gray-600">No tienes productos</p>
+                    )}
+                  </div>
+                )}
               </div>
             </div>
           </div>
