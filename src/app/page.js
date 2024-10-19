0f7cbaa16c720c1daad869518dd2655244c86d71
Yohendry <yohendry.hurtado@tafitech.io>
2024-10-19 11:39:38 -0500

    implementacion de llamad a API

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,15 @@
 "use client";
-import { ProductCard } from "../components/ProductCard/ProductCard";
-import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+const ProductPanel = dynamic(() => import("../components/ProductPanel"), {
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Home() {
-  const [productos, setProductos] = useState([]);
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((productos) => setProductos(productos));
-  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
-        {productos.map((producto, index) => (
-          <ProductCard
-            key={index}
-            name={producto.title}
-            description={producto.description}
-            price={producto.price}
-            imageUrl={producto.image}
-          />
-        ))}
+      <main className="grid grid-cols-4 gap-6 row-start-2 items-center sm:items-start">
+        <ProductPanel />
       </main>
     </div>
   );
