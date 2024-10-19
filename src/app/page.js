0e8c50014d871c20db77c07006db1d53ec025ca8
Yohendry <yohendry.hurtado@tafitech.io>
2024-10-19 11:04:50 -0500

    implementar fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,24 @@
-import Image from "next/image";
+"use client";
 import { ProductCard } from "../components/ProductCard/ProductCard";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const pokemons = [
-    {
-      name: "Eevee",
-      description: "Eevee de color marron",
-      price: 200,
-      imageUrl: "/assets/images/evee.png",
-    },
-    {
-      name: "Pikachu",
-      description: "Pikachu de color amarillo",
-      price: 300,
-      imageUrl: "/assets/images/pikachu.png",
-    },
-    {
-      name: "Charmander",
-      description: "Charmander de color naranja",
-      price: 400,
-      imageUrl: "/assets/images/charmander.png",
-    },
-  ];
+  const [productos, setProductos] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((productos) => setProductos(productos));
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
-        {pokemons.map((pokemon, index) => (
+        {productos.map((producto, index) => (
           <ProductCard
             key={index}
-            name={pokemon.name}
-            description={pokemon.description}
-            price={pokemon.price}
-            imageUrl={pokemon.imageUrl}
+            name={producto.title}
+            description={producto.description}
+            price={producto.price}
+            imageUrl={producto.image}
           />
         ))}
       </main>
